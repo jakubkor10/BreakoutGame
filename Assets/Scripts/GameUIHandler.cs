7201adfcf31cbe27d4c8a13ffc84fc205da10162
jakubkor10 <jakubkor10@gmail.com>
2023-07-28 18:11:19 +0200

    Bugfix
    - highscore updates correctly (open for future optimalization)

--- Assets/Scripts/GameUIHandler.cs
+++ Assets/Scripts/GameUIHandler.cs
@@ -7,12 +7,26 @@ public class GameUIHandler : MonoBehaviour
 {
     public GameManager manager;
     public Text HighScoreText;
+    public int currentHighscore;
     private void Awake()
     {
         manager = FindObjectOfType<GameManager>();
     }
     void Start()
     {
+        currentHighscore = manager.highScore;
+        UpdateHighscore();
+    }
+    private void Update()
+    {
+        if (manager.highScore != currentHighscore)
+        {
+            UpdateHighscore();
+        }
+    }
+    public void UpdateHighscore()
+    {
         HighScoreText.text = $"HighestScore:{manager.highScore} Player: {manager.bestPlayer}";
+        currentHighscore = manager.highScore;
     }
 }
